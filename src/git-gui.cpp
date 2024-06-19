e0739d424e0f92e0b0d22574b2ba0e9c9b70dd1c
N. Pattakos <pattakosn@fastmail.com>
2024-06-19 17:29:30 +0200

    init/shutdown libgit2

--- src/git-gui.cpp
+++ src/git-gui.cpp
@@ -1,7 +1,18 @@
+// https://libgit2.org/docs/guides/101-samples/
 #include <iostream>
+#include <git2/global.h>
 
 int main()
 {
-  std::cout << "git gui\n";
-  return EXIT_SUCCESS;
+        std::cout << "git gui\n";
+
+        std::cout << "libgit2: Initializing\n";
+        git_libgit2_init();
+        std::cout << "libgit2: Initializes\n";
+
+        std::cout << "libgit2: Shutting down\n";
+        git_libgit2_shutdown();
+        std::cout << "libgit2: Shut down\n";
+        
+        return EXIT_SUCCESS;
 }
