3ff6e0c0f95c72a86330598e1762e07eda22d173
N. Pattakos <pattakosn@fastmail.com>
2024-06-21 23:51:27 +0200

    init/deinit lib, and search for repo in path

--- src/git-gui.cpp
+++ src/git-gui.cpp
@@ -1,32 +1,17 @@
 // https://libgit2.org/docs/guides/101-samples/
 #include <iostream>
-
-
-
+#include "mygit.h"
 
 int main()
 {
-        std::cout << "git gui\n";
-
-        std::cout << "libgit2: Initializing\n";
-        int error = git_libgit2_init();
-        check_error(error);
-        std::cout << "libgit2: Initialized with error: " << error << "\n";
-
-        std::cout << "libgit2: Initializing again\n";
-        error = git_libgit2_init();
-        check_error(error);
-        std::cout << "libgit2: Initialized again with error: " << error << "\n";
-
-        
-        git_buf root = {0};
-        error = git_repository_discover(&root, ".", 1, "../../../");
-        check_error(error);
-        printf("found repo at: %s\n", root.ptr);
-        git_buf_dispose(&root); /* returned path data must be freed after use */
-
-
+        std::cout << "\n\tgit gui 0.0.0\n";
         atexit(deinit);
+        atexit(deinit);
+        atexit(deinit);
+        init();
+        init();
+
+        find_repo();
        
         return EXIT_SUCCESS;
 }
